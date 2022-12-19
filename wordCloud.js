99bf3487c285f42296c418313d0772fc3cca0dd4
Joe Napolitan <napolitanjoe@gmail.com>
2022-12-19 16:29:28 -0500

    Add word counter

--- wordCloud.js
+++ wordCloud.js
@@ -1,9 +1,47 @@
 const submit = document.getElementById("submitButton")
 
-submit.addEventListener("click", () => createWordArray());
+submit.addEventListener("click", () => createWordCloud());
 
+// Adds each word in the paragraph entered to an array
 function createWordArray() {
     let paragraph = document.getElementById("paragraphInput")
     let words = paragraph.value.split(' ');
-    console.log(words)
+    return words;
+}
+
+// Returns an array containing word objects, 
+// indicating the name of the word and the number of occurances
+function createWordCloud() {
+    let words = createWordArray();
+    let checkedWords = []
+    
+    // Checks number of occurances
+    function countWord(word) {
+        count = 0
+        words.forEach((i => {
+            if (i === word) {
+                count+= 1;
+            }
+        }))
+        return count;
+    }
+
+    // Appends to new array while checking for duplicates
+    words.forEach((word) => {
+        let newWord = new WordCount(word,countWord(word))
+        const index = checkedWords.findIndex(i => i.name === newWord.name)
+        if (index === -1) {
+            checkedWords.push(newWord)
+        }
+    })
+
+    console.log(checkedWords)
+    return checkedWords;
+}
+
+class WordCount {
+    constructor(word,count) {
+        this.name = word;
+        this.count = count;
+    }
 }
